95d8b88ce29cb1dd808f072f9af4d4b764ed69c7
TereraiQubed <tapiwa.tererai@qubedlab.com>
2021-07-16 01:17:12 +0200

    template added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from quanticalabs.com/Medicenter/Template/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 14 Jul 2021 12:19:48 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <title>MediCenter - Medical Health Template</title>
+    <!--meta-->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="keywords" content="Medic, Medical Center" />
+    <meta name="description" content="Health Medical Clinic Template" />
+    <!--style-->
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Volkhov:400italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/reset.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/superfish.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/fancybox/jquery.fancybox.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/jquery.qtip.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/jquery-ui-1.9.2.custom.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/style.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/responsive.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/style/animations.css') }}" />
+    <link rel="shortcut icon" href="{{ asset('template/images/favicon.ico') }}" />
+    <!--js-->
+    <script type="text/javascript" src="{{ asset('template/js/jquery-1.12.4.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery-migrate-1.4.1.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.ba-bbq.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery-ui-1.9.2.custom.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.easing.1.3.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.carouFredSel-5.6.4-packed.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.sliderControl.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.timeago.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.hint.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.isotope.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.isotope.masonry.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.fancybox-1.3.4.pack.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.qtip.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/jquery.blockUI.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('template/js/main.js') }}"></script>
+</head>
+
+<body>
+    @include('partials.header')
+    @yield('content')
+    @include('partials.footer')
+</body>
+
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,229 @@
+<div class="footer_container">
+    <div class="footer">
+        <ul class="footer_banner_box_container clearfix">
+            <li class="footer_banner_box super_light_blue animated_element animation-fadeIn duration-500">
+                <h2>
+                    Health Insurance
+                </h2>
+                <p>
+                    <a class="icon_small_arrow right_white" href="#">Here in medicenter we have individual</a>
+                </p>
+            </li>
+            <li class="footer_banner_box light_blue animated_element animation-slideRight duration-800 delay-250">
+                <h2>
+                    Medical Records
+                </h2>
+                <p>
+                    <a class="icon_small_arrow right_white" href="#">Here in medicenter we have individual</a>
+                </p>
+            </li>
+            <li class="footer_banner_box blue animated_element animation-slideRight200 duration-800 delay-500">
+                <h2>
+                    Online Bill Pay
+                </h2>
+                <p>
+                    <a class="icon_small_arrow right_white" href="#">Here in medicenter we have individual</a>
+                </p>
+            </li>
+        </ul>
+        <div class="footer_box_container clearfix">
+            <div class="footer_box">
+                <h3 class="box_header">
+                    Medicenter Clinic
+                </h3>
+                <p class="info">
+                    Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada individual.
+                </p>
+                <ul class="footer_contact_info_container clearfix">
+                    <li class="footer_contact_info_row">
+                        <div class="footer_contact_info_left">
+                            Medicenter Clinic
+                        </div>
+                    </li>
+                    <li class="footer_contact_info_row">
+                        <div class="footer_contact_info_left">
+                            33 Farlane Street
+                        </div>
+                        <div class="footer_contact_info_right">
+                            +123 655 655
+                        </div>
+                    </li>
+                    <li class="footer_contact_info_row">
+                        <div class="footer_contact_info_left">
+                            Keilor East
+                        </div>
+                        <div class="footer_contact_info_right">
+                            +123 755 755
+                        </div>
+                    </li>
+                    <li class="footer_contact_info_row">
+                        <div class="footer_contact_info_left">
+                            VIC 3033, Australia
+                        </div>
+                        <div class="footer_contact_info_right">
+                            <a href="mailto:medicenter@mail.com" title="Send Email">
+                                medicenter@mail.com
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="social_icons clearfix">
+                    <li>
+                        <a class="social_icon facebook" href="http://facebook.com/QuanticaLabs" title=""
+                            target="_blank">
+                            &nbsp;
+                        </a>
+                    </li>
+                    <li>
+                        <a class="social_icon twitter" href="https://twitter.com/QuanticaLabs" title="" target="_blank">
+                            &nbsp;
+                        </a>
+                    </li>
+                    <li>
+                        <a class="social_icon googleplus" href="#" title="">
+                            &nbsp;
+                        </a>
+                    </li>
+                    <li>
+                        <a class="social_icon mail" href="mailto:medicenter@mail.com" title="">
+                            &nbsp;
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer_box">
+                <div class="clearfix">
+                    <div class="header_left">
+                        <h3 class="box_header">
+                            Latest Posts
+                        </h3>
+                    </div>
+                    <div class="header_right">
+                        <a href="#" id="footer_recent_posts_prev"
+                            class="scrolling_list_control_left icon_small_arrow left_white"></a>
+                        <a href="#" id="footer_recent_posts_next"
+                            class="scrolling_list_control_right icon_small_arrow right_white"></a>
+                    </div>
+                </div>
+                <div class="scrolling_list_wrapper">
+                    <ul class="scrolling_list footer_recent_posts">
+                        <li class="icon_small_arrow right_white">
+                            <a href="indexb878.html?page=post">
+                                Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada.
+                            </a>
+                            <abbr title="29 May 2012" class="timeago">29 May 2012</abbr>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <a href="indexb878.html?page=post">
+                                Lorem ipsum dolor sit amat velum.
+                            </a>
+                            <abbr title="04 Apr 2012" class="timeago">04 Apr 2012</abbr>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <a href="indexb878.html?page=post">
+                                Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada.
+                            </a>
+                            <abbr title="02 Feb 2012" class="timeago">02 Feb 2012</abbr>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <a href="indexb878.html?page=post">
+                                Lorem ipsum dolor sit amat velum, consectetur adipiscing elit.
+                            </a>
+                            <abbr title="24 Jan 2011" class="timeago">24 Jan 2011</abbr>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer_box last">
+                <div class="clearfix">
+                    <div class="header_left">
+                        <h3 class="box_header">
+                            Latest Tweets
+                        </h3>
+                    </div>
+                    <div class="header_right">
+                        <a href="#" id="latest_tweets_prev"
+                            class="scrolling_list_control_left icon_small_arrow left_white"></a>
+                        <a href="#" id="latest_tweets_next"
+                            class="scrolling_list_control_right icon_small_arrow right_white"></a>
+                    </div>
+                </div>
+                <div class="scrolling_list_wrapper">
+                    <ul class="scrolling_list latest_tweets">
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v2.0) of our Pressroom #News #Magazine #HTML #Template Please
+                                check at #ThemeForest <a href="https://t.co/8dumriEjeE"
+                                    target="_blank">t.co/8dumriEjeE</a><abbr title="2021-06-30T14:03:08+00:00"
+                                    class="timeago">Wed Jun 30 14:03:08 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We have released new version (v2.7) of our Renovate #Construction #Renovation #HTML
+                                #Template Check at #ThemeForest <a href="https://t.co/dnS0uJuL90"
+                                    target="_blank">t.co/dnS0uJuL90</a><abbr title="2021-06-28T14:33:44+00:00"
+                                    class="timeago">Mon Jun 28 14:33:44 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v1.6) of our #CleanMate #Cleaning Company #Maid #Gardening
+                                #HTML Template. Please chec… <a href="https://t.co/U2pPJo8AAF"
+                                    target="_blank">t.co/U2pPJo8AAF</a><abbr title="2021-06-24T14:53:39+00:00"
+                                    class="timeago">Thu Jun 24 14:53:39 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v6.4) of our #Car Service #Mechanic #Auto #Shop #WordPress
+                                Theme. Check at #ThemeForest <a href="https://t.co/GDaLlbxZaS"
+                                    target="_blank">t.co/GDaLlbxZaS</a><abbr title="2021-06-22T13:28:38+00:00"
+                                    class="timeago">Tue Jun 22 13:28:38 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v6.9) of our Renovate #Construction #Renovation #WordPress
+                                Theme. Check at #ThemeForest <a href="https://t.co/Qq0xOm69tZ"
+                                    target="_blank">t.co/Qq0xOm69tZ</a><abbr title="2021-06-21T09:49:18+00:00"
+                                    class="timeago">Mon Jun 21 09:49:18 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We have released new version (v3.0) of our Car Service - #Mechanic #Auto #Shop #HTML
+                                #Template Please check at… <a href="https://t.co/Mfmol61q1d"
+                                    target="_blank">t.co/Mfmol61q1d</a><abbr title="2021-06-18T16:06:06+00:00"
+                                    class="timeago">Fri Jun 18 16:06:06 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v14.2) of our GymBase #Gym #Fitness #WordPress Theme. Please
+                                check at #ThemeForest <a href="https://t.co/8mEwDr0hie"
+                                    target="_blank">t.co/8mEwDr0hie</a><abbr title="2021-06-17T13:14:46+00:00"
+                                    class="timeago">Thu Jun 17 13:14:46 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v5.3) of our Pressroom #News #Magazine #WordPress Theme.
+                                Please check at #ThemeForest <a href="https://t.co/VSiuakDbGa"
+                                    target="_blank">t.co/VSiuakDbGa</a><abbr title="2021-06-17T11:41:20+00:00"
+                                    class="timeago">Thu Jun 17 11:41:20 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v13.1) of our MediCenter #Health #Medical #Clinic #WordPress
+                                Theme. Please check at… <a href="https://t.co/DIQU9OuAFU"
+                                    target="_blank">t.co/DIQU9OuAFU</a><abbr title="2021-06-15T15:30:05+00:00"
+                                    class="timeago">Tue Jun 15 15:30:05 +0000 2021</abbr></p>
+                        </li>
+                        <li class="icon_small_arrow right_white">
+                            <p>We've released new version (v6.8) of our Renovate #Construction #Renovation #WordPress
+                                Theme. Check at #ThemeForest <a href="https://t.co/Qq0xOm69tZ"
+                                    target="_blank">t.co/Qq0xOm69tZ</a><abbr title="2021-06-15T13:35:12+00:00"
+                                    class="timeago">Tue Jun 15 13:35:12 +0000 2021</abbr></p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="copyright_area clearfix">
+            <div class="copyright_left">
+                © Copyright - <a
+                    href="https://themeforest.net/item/medicenter-responsive-medical-health-template/4000598?ref=QuanticaLabs"
+                    title="MediCenter Template" target="_blank">MediCenter Template</a> by <a
+                    href="http://quanticalabs.com/" title="QuanticaLabs" target="_blank">QuanticaLabs</a>
+            </div>
+            <div class="copyright_right">
+                <a class="scroll_top icon_small_arrow top_white" href="#top" title="Scroll to top">Top</a>
+            </div>
+        </div>
+    </div>
+</div>
